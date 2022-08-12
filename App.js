574ecf68e915a759b844a013b1910636d66cb9ae
Kaan KIVIRCIK <kaankvrck@gmail.com>
2022-08-12 17:32:27 +0300

    Car Item component

--- App.js
+++ App.js
@@ -1,55 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import CarItem from "./components/CarItem";
 
 export default function App() {
-  return (<View style={styles.container}>
+    return (
+        <View style={styles.container}>
 
-    <View style={styles.carContainer}>
+            <CarItem/>
 
-      <ImageBackground source={require('./assets/images/ModelX.jpeg')} style={styles.image}/>
-
-      <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at 69,420</Text>
-      </View>
-
-
-    </View>
-
-    <StatusBar style="auto"/>
-  </View>);
+            <StatusBar style="auto"/>
+        </View>);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
-  },
+    container: {
+        flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center',
+    },
 
-  carContainer: {
-    width: '100%',
-    height: '100%'
-  },
-
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: '600'
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#767676',
-
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: "cover",
-    position: "absolute"
-
-  }
 
 });
