e54d2550565ab84faa344ef5c7b4dc8eead52525
Kaan KIVIRCIK <kaankvrck@gmail.com>
2022-08-13 16:30:01 +0300

    Snaps and Flatlist updated

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import {StatusBar} from 'expo-status-bar';
+import React from 'react';
 import {ImageBackground, StyleSheet, Text, View} from 'react-native';
-import CarItem from "./components/CarItem";
+import CarsList from "./components/ CarsList";
 
 export default function App() {
     return (
         <View style={styles.container}>
-
-            <CarItem
-                name={"Model 3"}
-                tagline={"Order Online for"}
-                taglineCTA={"Touchless Delivery"}
-                image={require('./assets/images/Model3.jpeg')}
-            />
-
+            <CarsList/>
             <StatusBar style="auto"/>
         </View>);
 }
---
+++ components/ CarsList/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Dimensions, FlatList, Text, View} from 'react-native';
+import cars from "./cars";
+import styles from './styles';
+import CarItem from "../CarItem";
+
+const CarsList = (props) => {
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={cars}
+                renderItem={({item}) => <CarItem car={item}/>}
+                snapToAlignment={'start'}
+                decelerationRate={'fast'}
+                snapToInterval={Dimensions.get('window').height}
+                showsVerticalScrollIndicator={false}
+            />
+        </View>
+    );
+};
+
+export default CarsList;
